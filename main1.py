3b033234e87cc2103f7b5d1c3618287fcb0922a1
Secale cereale <deinla.bec@obmontessori.edu.ph>
2026-05-07 08:27:35 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,12 +1,11 @@
 from pyscript import document
 
 def log():
-    suj=(document.getElementById("sec"))
-    sju=""
-    sj=(document.getElementById("sub"))
-    su=""
-    nae=(document.getElementById("na"))
+    suj = document.getElementById("sec").value
+    sj = document.getElementById("sub").value
+    nae = document.getElementById("na").value
 
+    # Section
     if suj == "1":
         sju = "Emerald"
     elif suj == "2":
@@ -14,8 +13,9 @@ def log():
     elif suj == "3":
         sju = "Sapphire"
     else:
-        sju="Topaz"
+        sju = "Topaz"
 
+    # Subject
     if sj == "1":
         su = "English"
     elif sj == "2":
@@ -30,21 +30,25 @@ def log():
         su = "ICT"
     elif sj == "7":
         su = "TLE"
-    elif sj == "7":
+    elif sj == "8":
         su = "VE"
     elif sj == "9":
         su = "LT/CAT"
     else:
-        su="FREE TIME"
-
+        su = "FREE TIME"
 
     class ME:
-        def __init__(self,nam,sec,sub):
+        def __init__(self, nam, sec, sub):
             self.nam = nam
             self.sec = sec
             self.sub = sub
-        
 
-    me1 = ME(nae,su,sju)
-    log = "Hello! I am "+me1.nam+" from "+me1.sec+" and my fav subject is "+me1.sub+"."
-    document.getElementById("logg").innerText = log
+    me1 = ME(nae, sju, su)
+
+    log_text = (
+        "Hello! I am " + me1.nam +
+        " from " + me1.sec +
+        " and my fav subject is " + me1.sub + "."
+    )
+
+    document.getElementById("logg").innerText = log_text
