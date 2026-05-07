ab09b5504953c09ddcb774a608dd606b0225502a
Secale cereale <deinla.bec@obmontessori.edu.ph>
2026-05-07 16:45:04 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -3,8 +3,8 @@ import matplotlib
 matplotlib.use("module://matplotlib_pyodide.html5_canvas_backend")
 
 import matplotlib.pyplot as plt
-import numpy as np
 
+# Store data
 days = []
 absences = []
 
@@ -14,17 +14,24 @@ def displaying(event):
     day = document.getElementById("day").value
     absence = int(document.getElementById("absence").value)
 
+    # Save data
     days.append(day)
     absences.append(absence)
 
+    # Clear old graph
     plt.clf()
 
-    plt.plot(days, absences, marker="o")
+    # Create figure
+    fig, ax = plt.subplots()
 
-    plt.title("Weekly Attendance (Absences)")
-    plt.xlabel("Day")
-    plt.ylabel("Number of Absences")
+    # Plot data
+    ax.plot(days, absences, marker="o")
 
-    plt.grid()
+    ax.set_title("Weekly Attendance (Absences)")
+    ax.set_xlabel("Day")
+    ax.set_ylabel("Number of Absences")
 
-    plt.show()
+    ax.grid(True)
+
+    # IMPORTANT
+    display(fig, target="graph")
